db9a1cd03a03dc0cae3b6e7bd35160e8c52ec6e2
tomer101011 <51194603+tomer101011@users.noreply.github.com>
2020-09-14 06:40:20 +0000

    Deploying to gh-pages from  @ 8d98845bf2420d2180056de5de0469b636401e7c 🚀

---
+++ precache-manifest.417d63a7510a8022a5dddc127e6e7a20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff91cbadac8f838d99e39d13d46bc8e3",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "3039b1b1874af3336f7a",
+    "url": "/personal-website/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "aab353fd5777e279ac6d",
+    "url": "/personal-website/static/css/main.7a270926.chunk.css"
+  },
+  {
+    "revision": "3039b1b1874af3336f7a",
+    "url": "/personal-website/static/js/2.840e665a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website/static/js/2.840e665a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab353fd5777e279ac6d",
+    "url": "/personal-website/static/js/main.0c5e08a3.chunk.js"
+  },
+  {
+    "revision": "d9385b05e5e57d9628fe",
+    "url": "/personal-website/static/js/runtime-main.3731dab7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.b8fe27da947147d199898b9cfe27886e.js"
+  "/personal-website/precache-manifest.417d63a7510a8022a5dddc127e6e7a20.js"
 );
 
 self.addEventListener('message', (event) => {
