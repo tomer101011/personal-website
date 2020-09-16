758cc0688124478925741104ca84e1fa894e12cd
tomer101011 <51194603+tomer101011@users.noreply.github.com>
2020-09-16 11:10:32 +0000

    Deploying to gh-pages from  @ 22a6e862e2f05d5e272cc7e2ef6bd33081b65a2f 🚀

---
+++ precache-manifest.5b32829d186595cc71903899765c8387.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5759bc0658a45187b7cb1e9e80b802",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "1b030353ad1be8790512",
+    "url": "/personal-website/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "56a997feea428fc9b8a4",
+    "url": "/personal-website/static/css/main.7431f8a3.chunk.css"
+  },
+  {
+    "revision": "1b030353ad1be8790512",
+    "url": "/personal-website/static/js/2.5fcc0166.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/personal-website/static/js/2.5fcc0166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a997feea428fc9b8a4",
+    "url": "/personal-website/static/js/main.eb68eb22.chunk.js"
+  },
+  {
+    "revision": "d9385b05e5e57d9628fe",
+    "url": "/personal-website/static/js/runtime-main.3731dab7.js"
+  },
+  {
+    "revision": "d2290058511ac15232922a68fc2ed7df",
+    "url": "/personal-website/static/media/shoe.d2290058.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.46bd3db5823a2859ff1a8e37b1aa2546.js"
+  "/personal-website/precache-manifest.5b32829d186595cc71903899765c8387.js"
 );
 
 self.addEventListener('message', (event) => {
