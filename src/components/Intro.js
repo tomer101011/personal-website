3f0ee5dfa39c82710ac644546912edc7b8eee816
Tomer <tomerst3@gmail.com>
2020-09-16 14:45:02 +0300

    Continue

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,7 +8,7 @@ export default class Intro extends Component {
             <div>
                 <Image className="img-style" src={require(`../pictures/shoe.jpg`)} rounded />
 
-                <div className="caption">
+                <div className="caption head">
                     <p id="text1">Hello, I'm</p>
                     <h1 id="text2">Tomer Steiner</h1>
                     <p id="text3">and this is my resume</p>
